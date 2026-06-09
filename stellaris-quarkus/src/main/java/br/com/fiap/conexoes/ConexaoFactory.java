8f347b40f25960d34b2b977b48dc35d930021289
julianasrmarques <teacherjulim@gmail.com>
2026-06-09 18:45:30 -0300

    banana

--- stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -9,6 +9,10 @@ public class ConexaoFactory {
     // metodo de conexão com o banco de dados
     public Connection conexao() throws ClassNotFoundException, SQLException {
         String url = System.getenv("DATABASE_URL");
+        // Converte o formato da URL para JDBC
+        if (url != null && url.startsWith("postgresql://")) {
+            url = url.replace("postgresql://", "jdbc:postgresql://");
+        }
         return DriverManager.getConnection(url);
     }
 }
