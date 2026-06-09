5def9f7d3b09ab58be6a7e763af5344b4d4d1512
julianasrmarques <teacherjulim@gmail.com>
2026-06-09 19:27:29 -0300

    ajuste

--- stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -8,8 +8,22 @@ public class ConexaoFactory {
 
     // metodo de conexão com o banco de dados
     public Connection conexao() throws ClassNotFoundException, SQLException {
-        String url = System.getenv("DATABASE_URL");
-        url = "jdbc:" + url;
-        return DriverManager.getConnection(url);
+        try {
+            String dbUrl = System.getenv("DATABASE_URL");
+            java.net.URI uri = new java.net.URI(dbUrl);
+            String host = uri.getHost();
+            int port = uri.getPort();
+            String path = uri.getPath();
+            String userInfo = uri.getUserInfo();
+            String user = userInfo.split(":")[0];
+            String password = userInfo.split(":")[1];
+            String query = uri.getQuery();
+
+            String jdbcUrl = "jdbc:postgresql://" + host + ":" + port + path + (query != null ? "?" + query : "");
+
+            return DriverManager.getConnection(jdbcUrl, user, password);
+        } catch (java.net.URISyntaxException e) {
+            throw new SQLException("URL inválida: " + e.getMessage());
+        }
     }
 }
