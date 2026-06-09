1f700a07d6389125026b2f51e63ab8f095d75d4d
julianasrmarques <teacherjulim@gmail.com>
2026-06-09 18:56:19 -0300

    espero que funcione

--- stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -10,20 +10,8 @@ public class ConexaoFactory {
     public Connection conexao() throws ClassNotFoundException, SQLException {
         String url = System.getenv("DATABASE_URL");
 
-        // Converte postgresql://user:pass@host:port/db para jdbc:postgresql://host:port/db
         if (url != null && url.startsWith("postgresql://")) {
-            url = url.replace("postgresql://", "jdbc:postgresql://");
-            // Remove credenciais da URL e passa separadamente
-            java.net.URI uri = java.net.URI.create(url.replace("jdbc:", ""));
-            String host = uri.getHost();
-            int port = uri.getPort();
-            String db = uri.getPath().substring(1);
-            String userInfo = uri.getUserInfo();
-            String user = userInfo.split(":")[0];
-            String pass = userInfo.split(":")[1];
-
-            url = "jdbc:postgresql://" + host + ":" + port + "/" + db;
-            return DriverManager.getConnection(url, user, pass);
+            url = "jdbc:" + url;
         }
 
         return DriverManager.getConnection(url);
