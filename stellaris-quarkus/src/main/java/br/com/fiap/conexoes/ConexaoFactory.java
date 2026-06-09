1e29935542fa9c2cd1fa32c088b6e8f8ef5cd204
julianasrmarques <teacherjulim@gmail.com>
2026-06-09 19:05:06 -0300

    n

--- stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ stellaris-quarkus/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -12,10 +12,10 @@ public class ConexaoFactory {
 
         if (url != null && url.startsWith("postgresql://")) {
             url = url.replace("postgresql://", "jdbc:postgresql://");
-            // Adiciona porta padrão se não tiver
             if (!url.matches(".:\\d+/.")) {
                 url = url.replaceFirst("(jdbc:postgresql://[^/]+)", "$1:5432");
             }
+            url = url + "?sslmode=require";
         }
 
         return DriverManager.getConnection(url);
